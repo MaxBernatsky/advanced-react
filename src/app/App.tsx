55c48b69728b190b3fe462c7532ab8828afc09ee
Maxim Bernatsky <maxsateon@yandex.ru>
2023-07-22 21:07:55 +0300

    project refactor under feature sliced design

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import React, {Suspense} from 'react';
 import './styles/index.scss'
 import {Link, Route, Routes} from 'react-router-dom';
-import {MainPageLazy} from './pages/MainPage/MainPage.lazy';
-import {AboutPageLazy} from './pages/AboutPage/AboutPage.lazy';
-import {useTheme} from './theme/useTheme';
-import {classNames} from './helpers/classNames/classNames';
+import {useTheme} from 'app/providers/ThemeProvider';
+import {classNames} from 'shared/lib/classNames/classNames';
+import {AboutPage} from 'pages/AboutPage';
+import {MainPage} from 'pages/MainPage';
 
 
 const App = () => {
@@ -18,8 +18,8 @@ const App = () => {
             <Link to={'/about'}>About</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageLazy/>}/>
-                    <Route path={'/'} element={<MainPageLazy/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/'} element={<MainPage/>}/>
                 </Routes>
             </Suspense>
         </div>
