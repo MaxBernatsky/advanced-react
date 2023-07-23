8243fa80ea360a17f3d7d1059ce7c0e33520ee66
Maxim Bernatsky <maxsateon@yandex.ru>
2023-07-23 21:46:38 +0300

    feat: create navbar ui component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './styles/index.scss'
-import {Link} from 'react-router-dom';
 import {useTheme} from 'app/providers/ThemeProvider';
 import {classNames} from 'shared/lib/classNames/classNames';
 import {AppRouter} from 'app/providers/router';
+import {Navbar} from 'widgets/Navbar';
 
 
 const App = () => {
@@ -12,10 +12,9 @@ const App = () => {
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>Toggle Theme</button>
-            <Link to={'/'}>Main</Link>
-            <Link to={'/about'}>About</Link>
+            <Navbar/>
             <AppRouter/>
+            <button onClick={toggleTheme}>Toggle Theme</button>
         </div>
     );
 };
