997c32ec95dcb414b21eb005678250e06c631bf0
Maxim Bernatsky <maxsateon@yandex.ru>
2023-07-25 01:11:49 +0300

    feat: add babel.config.json and extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,6 +4,21 @@ import {BuildOptions} from './types/config';
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
+    const babelLoader = {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+                'plugins': [
+                    ['i18next-extract', {locales: ['ru', 'en'], keyAsDefaultValue: true}],
+                ]
+            },
+
+        }
+    }
+
     const svgLoader = {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
@@ -42,6 +57,7 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     return [
         svgLoader,
         fileLoader,
+        babelLoader,
         typescriptLoader,
         cssLoader
     ]
