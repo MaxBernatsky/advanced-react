8684a1cf51ef8e1ec7d694368dafde5bc5375426
Maxim Bernatsky <maxsateon@yandex.ru>
2023-08-03 15:49:10 +0300

    feat: add Storybook

--- src/widgets/PageError/ui/PageError.tsx
+++ src/widgets/PageError/ui/PageError.tsx
@@ -7,7 +7,7 @@ export const PageError = () => {
     const { t } = useTranslation();
 
     const reloadPage = () => {
-        // eslint-disable-next-line no-restricted-globals
+    // eslint-disable-next-line no-restricted-globals
         location.reload();
     };
     return (
